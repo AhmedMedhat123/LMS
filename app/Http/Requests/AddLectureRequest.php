0ed4635e1f58fa9e29ec65d82bfc6a41b8f20164
AhmedMedhat123 <117312363+AhmedMedhat123@users.noreply.github.com>
2025-03-27 01:45:27 +0200

    fixing instructor adding lecture course

--- app/Http/Requests/AddLectureRequest.php
+++ app/Http/Requests/AddLectureRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class AddLectureRequest extends FormRequest
 {
@@ -24,7 +25,32 @@ class AddLectureRequest extends FormRequest
         return [
            'lecture_title' => ['required', 'string', 'max:255'],
             'url' => ['nullable'],
-            'content' => ['nullable', 'string'],
+            'content' => [
+                'required_without:video',
+                'string',
+                Rule::requiredIf(function () {
+                    return empty($this->file('video'));
+                }),
+                function ($attribute, $value, $fail) {
+                    if (!empty($value) && $this->hasFile('video')) {
+                        $fail('You can only submit content or video, not both.');
+                    }
+                },
+                'nullable'
+            ],
+            'video' => [
+                'required_without:content',
+                'max:10240',
+                Rule::requiredIf(function () {
+                    return empty($this->input('content'));
+                }),
+                function ($attribute, $value, $fail) {
+                    if (!empty($value) && !empty($this->input('content'))) {
+                        $fail('You can only submit video or content, not both.');
+                    }
+                },
+                'nullable'
+            ],
         ];
     }
 }
